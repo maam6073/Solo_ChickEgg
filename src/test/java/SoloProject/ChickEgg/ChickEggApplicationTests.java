da0994f31ec23ffab9a57fe3188c1ec283db43f3
maam6073 <maam10@naver.com>
2023-08-04 16:29:29 +0900

    init

---
+++ src/test/java/SoloProject/ChickEgg/ChickEggApplicationTests.java
@@ -0,0 +1,13 @@
+package SoloProject.ChickEgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickEggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
