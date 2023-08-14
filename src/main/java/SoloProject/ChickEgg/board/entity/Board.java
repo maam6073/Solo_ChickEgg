daf90ed271058996baa69320a15e067d99049f3a
DoHyeong_Kim <maam10@naver.com>
2023-08-14 11:44:29 +0900

    feat : MVC패턴 계층 기반 패키지 구조잡기
    MVC패턴 계층 기반 패키지 구조잡기

---
+++ src/main/java/SoloProject/ChickEgg/board/entity/Board.java
@@ -0,0 +1,11 @@
+package SoloProject.ChickEgg.board.entity;
+
+public class Board {
+    int id;
+    String title;
+    String body;
+    String comment;
+    long views;
+
+
+}
--- src/test/java/SoloProject/ChickEgg/ChickEggApplicationTests.java
+++ src/test/java/SoloProject/ChickEgg/ChickEggApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChickEggApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
