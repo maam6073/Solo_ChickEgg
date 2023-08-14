c9db9447d22c62c6d7cc300dd1db5334509e4f80
DoHyeong_Kim <maam10@naver.com>
2023-08-14 11:38:49 +0900

    Revert "feat : MVC패턴 계층 기반 패키지 구조잡기" (#2)
    This reverts commit 82cc0b2196a0421318e80d7caad215179aa4b413.

--- src/main/java/SoloProject/ChickEgg/board/controller/BoardController.java
+++
@@ -1,9 +0,0 @@
-package SoloProject.ChickEgg.board.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/boards")
-public class BoardController {
-}
